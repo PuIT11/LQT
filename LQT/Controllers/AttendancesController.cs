1ff319c8bb9b7ed20838d9b5b9396b6303b7beda
NAT <toidxbp02@gmail.com>
2023-03-31 11:15:59 +0700

    Add project files.

---
+++ LQT/Controllers/AttendancesController.cs
@@ -0,0 +1,41 @@
+﻿using LQT.DTO;
+using LQT.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LQT.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
+        {
+
+            var UserId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == UserId && a.CourseId == attendanceDto.CourseId))
+                return BadRequest("The Attendance already exists!");
+
+            var attendance = new Attendance
+            {
+                CourseId = attendanceDto.CourseId,
+                AttendeeId = UserId 
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ LQT/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LQT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LQT.ViewModels
+{
+    public class CoursesViewModel
+    {
+        public IEnumerable<Course> UpcommingCourses { set; get; }
+        public bool ShowAction { set; get; }
+    }
+}
